390d8dfba6ea909490fa594fb714443b6aebb905
Olivier <olivier.verrier@ynov.com>
2020-04-29 10:20:38 +0200

    Début front

---
+++ frontend/src/components/AppComponent/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-logo">
+      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span class="font-semibold text-xl tracking-tight">Builder Archero</span>
+    </div>
+    <div class="block lg:hidden" @click="isExpanded=!isExpanded">
+      <button class="navbar-menu-button">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" v-if="isExpanded">
+      <router-link to="/dashboard" class="navbar-dashboard-button" >DashBoard</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isExpanded: false,
+  }),
+  created() {
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize() {
+      if (window.innerWidth >= 1024) this.isExpanded = true;
+      else this.isExpanded = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~Style/components/navbar';
+</style>
---
+++ frontend/src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full">
+    <Sidebar></Sidebar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import Sidebar from './SideBar'
+
+export default {
+  components: {
+    Sidebar
+  }
+
+}
+</script>
+
+<style>
+</style>
