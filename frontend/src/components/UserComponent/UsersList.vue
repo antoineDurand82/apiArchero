973796076dd24815fe07670259cb55fa70a3b5bb
Olivier <olivier.verrier@ynov.com>
2020-05-01 11:24:13 +0200

    User List en construction

--- frontend/src/components/UserComponent/UsersList.vue
+++ frontend/src/components/UserComponent/UsersList.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
+    <h2>User List</h2>
+    <DataTable/>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    DataTable: () => import('@/components/Dashboard/DataTable'),
+  }
+  
 
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  h2{
+    @apply text-3xl font-semibold text-gray-700 mt-4 mx-40;
+  }
 </style>
