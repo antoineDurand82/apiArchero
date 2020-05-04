f9a504e801de2aee4452ee734556249b7b447d94
Olivier <olivier.verrier@ynov.com>
2020-05-05 01:31:10 +0200

    typo

--- backend/seeders/20200430172538-heroes.js
+++ backend/seeders/20200430172538-heroes.js
@@ -3,7 +3,7 @@ const db = require('../app/models/index')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await db.hero.create({fullName: "Meowgik", attack: 135, maxHp: 550, firstBuff: "ttack +50 (all heroes)", secondBuff: "Dodge +5%", thirdBuff: "Attack +4% (all heroes)", fourthBuff: "Healing Effect of Red Heart +30%", linkImage: "/images/heroes/Jigsaw_Clean.png"})
+    await db.hero.create({fullName: "Meowgik", attack: 135, maxHp: 550, firstBuff: "Attack +50 (all heroes)", secondBuff: "Dodge +5%", thirdBuff: "Attack +4% (all heroes)", fourthBuff: "Healing Effect of Red Heart +30%", linkImage: "/images/heroes/Jigsaw_Clean.png"})
     await db.hero.create({fullName: "Helix", attack: 125, maxHp: 620, firstBuff: "Critical Damage: +20% (All Heroes)", secondBuff: "Healing Effect of Red Hearts +20%", thirdBuff: "Max HP +5% (All Heroes)", fourthBuff: "Critical Hit Chance: +5%", linkImage: "/images/heroes/HelixCropped.png"})
     await db.hero.create({fullName: "Taranis", attack: 130, maxHp: 540, firstBuff: "Crit Chance +2% (All Heroes)", secondBuff: "Damage to Airborne Units +20%", thirdBuff: "Attack +4% (All Heroes)", fourthBuff: "Crit Damage +50%", linkImage: "/images/heroes/UnequipTaranisAnimationCropped.png"})
     await db.hero.create({fullName: "Phoren", attack: 130, maxHp: 500, firstBuff: "Critical Chance +2% (All Heroes)", secondBuff: "Projectile Resistance: +15%", thirdBuff: "Critical Chance +2% (All Heroes)", fourthBuff: "Critical Chance +5%", linkImage: "/images/heroes/UnequipPhorenAnimationCropped.png"})
